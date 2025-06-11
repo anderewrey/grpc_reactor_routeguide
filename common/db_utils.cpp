a94e0547844806cb71f7ecca989ec229afeda670
anderewrey <57828010+anderewrey@users.noreply.github.com>
2025-06-11 15:41:31 -0300

    Reduce copyright headers for SPDX-License-Identifier

--- common/db_utils.cpp
+++ common/db_utils.cpp
@@ -1,17 +1,6 @@
 ///
-/// Copyright 2024-2025 anderewrey.
-///
-/// Licensed under the Apache License, Version 2.0 (the "License");
-/// you may not use this file except in compliance with the License.
-/// You may obtain a copy of the License at
-///
-///     http://www.apache.org/licenses/LICENSE-2.0
-///
-/// Unless required by applicable law or agreed to in writing, software
-/// distributed under the License is distributed on an "AS IS" BASIS,
-/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-/// See the License for the specific language governing permissions and
-/// limitations under the License.
+/// SPDX-License-Identifier: Apache-2.0
+/// Copyright 2024-2025 anderewrey
 ///
 
 #include "common/db_utils.h"
@@ -23,9 +12,9 @@
 #include <string_view>
 #include <vector>
 
-#include <spdlog/spdlog.h>  // NOLINT(build/include_order)
 #include <glaze/glaze.hpp>
-#include <gflags/gflags.h>
+#include <gflags/gflags.h>  // NOLINT(build/include_order)
+#include <spdlog/spdlog.h>  // NOLINT(build/include_order)
 
 #include "generated/route_guide.grpc.pb.h"
 #include "proto/proto_utils.h"
